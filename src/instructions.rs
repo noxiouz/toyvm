8234396aaa89a647b0b848c59f8a91b9a3e603ce
Anton Tiurin <noxiouz@yandex.ru>
2020-04-30 13:06:28 +0100

    add missing file

---
+++ src/instructions.rs
@@ -0,0 +1,59 @@
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HLT,  // Halt
+    LOAD, // Load
+    ADD,
+    MUL,
+    SUB,
+    DIV,
+    JMP,
+    JMPF,
+    JMPB,
+    IGL, // Illegal
+         // TODO: make it trait to check size, registers and so on
+}
+
+impl From<u8> for Opcode {
+    fn from(v: u8) -> Opcode {
+        match v {
+            0 => Opcode::HLT,
+            1 => Opcode::LOAD,
+            2 => Opcode::ADD,
+            3 => Opcode::SUB,
+            4 => Opcode::MUL,
+            5 => Opcode::DIV,
+            6 => Opcode::JMP,
+            7 => Opcode::JMPF,
+            8 => Opcode::JMPB,
+            _ => Opcode::IGL,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+}
+
+impl Instruction {
+    fn new(opcode: Opcode) -> Instruction {
+        Instruction { opcode: opcode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hlt() {
+        let opcode = Opcode::HLT;
+        assert_eq!(opcode, Opcode::HLT);
+    }
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
